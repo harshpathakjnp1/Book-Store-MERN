4226471cac086db3ed17f78ba85830b586c1911a
Harsh Vardhan <155745954+harshpathakjnp1@users.noreply.github.com>
2024-04-14 15:30:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { ShowBook } from './Pages/ShowBook'
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={ } />
+      <Route path='/' element={ <Home/>} />
       <Route path='' element={ } />
       <Route path='' element={ } />
       <Route path='' element={ } />
