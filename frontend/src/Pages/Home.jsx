960c1849e8b506b0bf4ce4d7befb906f756d0467
Harsh Vardhan <155745954+harshpathakjnp1@users.noreply.github.com>
2024-04-22 21:50:47 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -27,13 +27,13 @@ export const Home = () => {
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8  ">Book List</h1>
         <Link to='/books/create'>
-          <MdOutlineAddBox className="text-sky-800 text-4xl"/>
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
 
       </div>
-      {loading ? (<Spinner/>) : (<table className="w-full border-separate border-spacing-2">
+      {loading ? (<Spinner />) : (<table className="w-full border-separate border-spacing-2">
         <thead>
-          
+
           <tr>
             <th className="border border-slate-600 rounded-md">No</th>
             <th className="border border-slate-600 rounded-md">Title</th>
@@ -41,15 +41,15 @@ export const Home = () => {
             <th className="border border-slate-600 rounded-md max-md:hidden">Publish Year</th>
             <th className="border border-slate-600 rounded-md ">Operations</th>
 
-            
+
 
           </tr>
         </thead>
         <tbody>
-          {books.map((book,index)=> {
+          {books.map((book, index) => {
             <tr key={book._id} className="h-8">
               <td className="border border-slate-700 rounded-md text-center">
-                {index+1}
+                {index + 1}
               </td>
               <td className="border border-slate-700 rounded-md text-center">
                 {book.title}
@@ -63,13 +63,13 @@ export const Home = () => {
               <td className="border border-slate-700 rounded-md text-center">
                 <div className="flex justify-center gap-x-4">
                   <Link to={`/books/details/${book._id}`}>
-                  <BsInfoCircle className="text-2xl text-green-800"/>
+                    <BsInfoCircle className="text-2xl text-green-800" />
                   </Link>
                   <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600"/>
+                    <AiOutlineEdit className="text-2xl text-yellow-600" />
                   </Link>
                   <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className="text-2xl text-red-600"/>
+                    <MdOutlineDelete className="text-2xl text-red-600" />
                   </Link>
                 </div>
               </td>
