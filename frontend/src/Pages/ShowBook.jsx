cd4278044a81391ed20eb139b835fd70cf7f153d
Harsh Vardhan <155745954+harshpathakjnp1@users.noreply.github.com>
2024-04-25 21:24:45 +0530

    update

--- frontend/src/Pages/ShowBook.jsx
+++ frontend/src/Pages/ShowBook.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../Components/BackButton'
+import { Spinner } from '../Components/Spinner'
 
 export const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false)
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://localhost:5555/books/${id}`).then((res) => {
+      setBook(res.data)
+      setLoading(false)
+    }).catch((error) => {
+      console.log(error)
+      setLoading(false)
+    })
+  }, [])
+
   return (
-    <div>ShowBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Book</h1>
+      {loading ? (<Spinner />) : (<div className='flex flex-col border-2 border-sky-400 w-fit rounded-xl p-4
+      '>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> Id</span>
+          <span>{book._id}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> Title</span>
+          <span>{book.title}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> Author</span>
+          <span>{book.author}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> PublishYear</span>
+          <span>{book.publishYear}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> Create Time</span>
+          <span>{new Date(book.createdAt).toString()}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'> Last Update Time</span>
+          <span>{new Date(book.updatedAt).toString()}</span>
+        </div>
+
+      </div>)}
+
+    </div>
   )
 }
