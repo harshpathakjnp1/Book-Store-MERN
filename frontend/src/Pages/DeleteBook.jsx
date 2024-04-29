e29ff17848052e485b40078a5dd81bc5be258e64
Harsh Vardhan <155745954+harshpathakjnp1@users.noreply.github.com>
2024-04-29 18:48:24 +0530

    Edit Book Delete Book added

--- frontend/src/Pages/DeleteBook.jsx
+++ frontend/src/Pages/DeleteBook.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import BackButton from '../Components/BackButton'
+import { Spinner } from '../Components/Spinner'
+import { useNavigate, useParams } from 'react-router-dom'
+
+
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const id = useParams()
+  const handleDeleteBook = () => {
+    setLoading(true)
+    axios.delete(`http://localhost:5555/books/${id}`).then(()=>{
+      setLoading(false)
+      navigate('/')
+    }).catch((error)=>{
+      setLoading(false)
+      alert('An Error Occured,please check console')
+      console.log(error)
+    })
+  }
   return (
-    <div>
+    <div className='p-4'>
+      <BackButton/>
       
+
     </div>
   )
 }
